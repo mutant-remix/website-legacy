68975b9f472f78d3cb2f5f1d9ce34d41a0b98fc2
wait-what <25844213+wait-what@users.noreply.github.com>
2023-05-05 20:55:32 +0300

    feat: add revolt invite

--- manifest.ts
+++ manifest.ts
@@ -1,3 +1,4 @@
 export const VERSION = '1.0';
 export const RELEASE_DATE = '05. May 2021';
-export const DISCORD_INVITE = 'https://discord.gg/tBNJfYdeBE';
+export const DISCORD_INVITE = 'https://discord.gg/SkM2XppTrR';
+export const REVOLT_INVITE = 'https://rvlt.gg/kcTDntf1';
